c4aa82ace47acab9b4d42bb88791b0edbf96b6d2
Ahmed Bougatf <98043267+bougatfa8@users.noreply.github.com>
2023-01-03 07:50:55 +0100

    Add files via upload

---
+++ src/app/demande-stage/demande-stage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DemandeStageComponent } from './demande-stage.component';
+
+describe('DemandeStageComponent', () => {
+  let component: DemandeStageComponent;
+  let fixture: ComponentFixture<DemandeStageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DemandeStageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DemandeStageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
